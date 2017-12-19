2445bae62803a686d5c0be3bbf402c7280c83035
joanneonis <joanne.onis@hotmail.com>
2017-12-20 00:48:30 +0100

    camera working!!

--- src/app/add-recept/add-recept.component.ts
+++ src/app/add-recept/add-recept.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit} from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 import { Subject} from 'rxjs/Subject';
-import {ActivatedRoute} from '@angular/router';
-import * as firebase from 'firebase';
+import {ActivatedRoute, Router} from '@angular/router';
+import { FirebaseApp } from 'angularfire2';
+import 'firebase/storage';
+import * as ImageCapture from 'image-capture';
 
 @Component({
 	selector: 'app-add-recept',
@@ -15,12 +18,14 @@ export class AddReceptComponent implements OnInit {
 		ingredienten: []
 	};
 	items: Array<any> = [];
+	videoPlayer;
+	imageBlob;
 
 	id: string;
 	private sub: any;
 
 
-	constructor(private af: AngularFireDatabase, private route: ActivatedRoute) {
+	constructor(private af: AngularFireDatabase, private route: ActivatedRoute, private fb: FirebaseApp, private router: Router) {
 		this.$items = af.list('/recepten', {
 			query: {
 				orderByChild: 'size'
@@ -38,6 +43,8 @@ export class AddReceptComponent implements OnInit {
 				this.af.object(`/recepten/${this.id}`).subscribe(a => this.newItem = a);
 			}
 		});
+
+		this.router.events.subscribe(a => this.stopStreaming());
 	}
 
 	add() {
@@ -46,7 +53,6 @@ export class AddReceptComponent implements OnInit {
 		} else {
 			this.$items.push(this.newItem);
 		}
-
 	}
 
 	addIngredient() {
@@ -57,25 +63,96 @@ export class AddReceptComponent implements OnInit {
 		});
 	}
 
-	 save(canvas) {
-		 console.log(canvas);
-    canvas.toBlob(blob => {
-      const storage = firebase.app().storage().ref();
-      const name = (new Date()).getTime() + '.png';
-      const f = storage.child('recept-images/' + name);
-      const task = f.put(blob);
-      task.on('state_changed', function(snapshot) {
-      }, function(error) {
-        console.error('Unable to save image.');
-        console.error(error);
-      }, function() {
-        const url = task.snapshot.downloadURL;
-        console.log('Saved to ' + url);
-      });
-    });
-  }
+
+
+ startVideo() {
+	 const that = this;
+	// enables the camera, if possible
+	if ('mediaDevices' in navigator) {
+		navigator.mediaDevices.getUserMedia({ video: true })
+			.then(function(stream){
+
+				// it will automatically ask for permission
+				// if the user denies, it will give back an error
+
+				// change the following querySelector to the id of the
+				// object where you want to send the stream of the video
+				that.videoPlayer = document.querySelector('#player');
+				that.videoPlayer.srcObject = stream;
+
+				// $('#divSelfie').css('display', 'block');
+
+				console.log(that.videoPlayer);
+
+			})
+			.catch(function(error){
+				// all errors here
+				console.log('There was an error', error);
+				// then show the image picker, because the camera doesn't work
+				// $('#pickImage').css('display', 'block')
+			});
+	}
+
+}
+ captureImage() {
+	 const that = this;
+	navigator.mediaDevices.getUserMedia({ video: true })
+			.then(function(stream){
+				const mediaStreamTrack = stream.getVideoTracks()[0];
+			  const imageCapture = new ImageCapture.ImageCapture(mediaStreamTrack);
+				const img = document.querySelector('img');
+				imageCapture.takePhoto()
+					.then(blob => {
+						that.imageBlob = blob;
+						img.src = URL.createObjectURL(blob);
+						img.onload = () => { URL.revokeObjectURL(img.src); };
+					})
+					.catch(error => console.error('takePhoto() error:', error));
+		});
+}
+
+ pickImage() {
+	this.stopStreaming();
+	// $('#pickImage').css('display', 'block');
+}
+ stopStreaming() {
+	if (this.videoPlayer) {
+		this.videoPlayer.srcObject.getVideoTracks() // gives access to the running video streams of the object
+			.forEach(function(track){
+				track.stop(); // stop all streams
+			});
+	}
+	// $('#divSelfie').css('display', 'none');
+}
+
+
+	save() {
+		const that = this;
+
+		 const storage = this.fb.storage().ref();
+		 const name = (new Date()).getTime() + '.png';
+		 const f = storage.child('recept-images/' + name);
+			 // blob hier
+		 const task = f.put(this.imageBlob);
+
+		 task.on('state_changed', function(snapshot) {
+		 }, function(error) {
+			 console.error('Unable to save image.');
+			 console.error(error);
+		 }, function() {
+				that.stopStreaming();
+			 const url = task.snapshot.downloadURL;
+			 console.log('Saved to ' + url);
+			 that.newItem.afbeeldingurl = url;
+
+			 that.add();
+		 });
+ }
 
 	trackByIndex(index: number, obj: any): any {
 		return index;
 	}
 }
+
+
+
