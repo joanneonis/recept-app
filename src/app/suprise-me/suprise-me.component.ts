6631d9b5e7c93a38fbc620733769b4ec700175f3
joanneonis <joanne.onis@hotmail.com>
2017-12-20 14:12:20 +0100

    styles suprise me

--- src/app/suprise-me/suprise-me.component.ts
+++ src/app/suprise-me/suprise-me.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import * as Shake from 'shake.js';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import {HostListener} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'app-suprise-me',
   templateUrl: './suprise-me.component.html',
-  styleUrls: ['./suprise-me.component.css']
+	styleUrls: ['./suprise-me.component.css']
 })
 export class SupriseMeComponent implements OnInit {
+	eventSubscription: any;
 
-  constructor() { }
+	$items: FirebaseListObservable<any[]>;
+	newItem = {ingredienten: []};
+	items: Array<any>= [];
+	randomItem = [];
+	ctrl = this;
+
+	constructor(af: AngularFireDatabase) {
+		this.$items = af.list('/recepten', {
+			query: {
+				orderByChild: 'size'
+			}
+		});
+	}
 
   ngOnInit() {
+			this.$items.subscribe(a => {
+				this.items = a;
+				this.randomRecept();
+			});
 
 			// create a new instance of shake.js.
 			const myShakeEvent = new Shake({
@@ -21,12 +42,16 @@ export class SupriseMeComponent implements OnInit {
 			myShakeEvent.start();
 
 			// register a shake event
-			window.addEventListener('shake', shakeEventDidOccur, false);
+			// window.addEventListener('shake', this.shakeEventDidOccur, false);
 
-			// shake event callback
-			function shakeEventDidOccur () {
-					// put your own code here etc.
-					alert('Shake!');
-			}
+			this.eventSubscription = Observable.fromEvent(window, 'shake').subscribe(e => {
+				this.randomRecept();
+				console.log('test');
+			});
 		}
+
+		randomRecept() {
+			this.randomItem = this.items[Math.floor(Math.random() * this.items.length)];
+		}
+
 }
